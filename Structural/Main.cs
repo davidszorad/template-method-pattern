a297f31d83764b499178288c61f4d4537e0c5330
David Szorad <dszorad@win.ips-ag.net>
2018-01-15 15:44:46 +0100

    variable renamed

--- Structural/Main.cs
+++ Structural/Main.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace TemplateMethodDesignPattern.Structural
 {
     /*
+    DEFINITION:
     Define the skeleton of an algorithm in an operation, deferring some steps to subclasses. 
     Template Method lets subclasses redefine certain steps of an algorithm without changing 
     the algorithm's structure. 
